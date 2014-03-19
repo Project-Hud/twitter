eb14be42101508eab4cde8014e26da41805385a5
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2014-03-19 16:06:42 +0000

    Implementing twitter feed

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 var Widget = new require('hud-widget')
   , widget = new Widget()
+  , getLatestTweet = require('./lib/latest-tweet') 
 
 widget.get('/', function (req, res) {
-  res.render('index', { title: '' })
+  getLatestTweet(function (err, tweet) {
+    if (err) {
+      console.log(err)
+      return res.send(500, { error: err })
+    }
+
+    res.render('index', { title: 'Twitter', tweet: tweet })  
+  })
+})
+
+widget.get('/latest', function () {
+  getLatestTweet(function (err, tweet) {
+    if (err) {
+      console.log(err)
+      return res.send(500, { error: err })
+    }
+    res.send(tweet)  
+  })
 })
